df609691990e335a8b514fcfb1df26454b4ee3d9
Alex M <33638869+WeirdAlex03@users.noreply.github.com>
2021-08-07 15:45:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("I have not yet made this portion, but save the link and your PIN for when this is done.")
+console.log("When this is finished, you'll \"sign in\" with your Discord tag and PIN, and then will be able to see your classmates in a format similar to this:")
+console.log(`
+Classmates for [your name here]
+	CSE 199 - B1
+		Same section & lab/recitation
+			John Smith (JSmith#1234), Jane Smith (JaneS#6789), Joe (JoeWho#6969)
+		Same class section, different lab/recitation
+			Name (user#tag)
+		Same class, different sections (and possibly different teacher)
+			Name (user#tag), Name (user#tag)
+
+	RUS 101 - B
+		Same section
+			Name (user#tag), Name (user#tag)
+		Same class (possibly different teacher)
+			Name (user#tag), Name (user#tag), Name (user#tag)
+`)
+console.log("This will eventually also be on a (very basic) website for some (slightly) better formatting, but this is the same information")
